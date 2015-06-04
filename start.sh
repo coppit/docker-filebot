87544195bc976b6becff108a91c0ab004296abfd
David Coppit <david@coppit.org>
2015-06-04 00:42:28 -0400

    Add timestamps to log output

--- start.sh
+++ start.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+function ts {
+  echo [`date '+%b %d %X'`]
+}
+
+echo "$(ts) Starting FileBot container"
+
 # Create filebot.sh unless there is already one
 if [ ! -f /config/filebot.sh ]; then
-  echo "Creating /config/filebot.sh and exiting"
+  echo "$(ts) Creating /config/filebot.sh and exiting"
   cp /root/filebot.sh /config/filebot.sh
   exit 1
 fi
 
 if test "`find /root/filebot.sh -newer /config/filebot.sh`"
 then
-  echo "ERROR: The container's filebot.sh is newer than the one in /config."
-  echo "  Copying the new script to /config/filebot.sh.new."
-  echo "  Compare your filebot.sh and filebot.sh.new. Save filebot.sh to reset its timestamp,"
-  echo "  then restart the container."
+  echo "$(ts) ERROR: The container's filebot.sh is newer than the one in /config."
+  echo "$(ts)   Copying the new script to /config/filebot.sh.new."
+  echo "$(ts)   Compare your filebot.sh and filebot.sh.new. Save filebot.sh to reset its timestamp,"
+  echo "$(ts)   then restart the container."
   cp /root/filebot.sh /config/filebot.sh.new
   exit 1
 fi
@@ -28,13 +34,13 @@ function run_filebot_sh {
 }
 
 # Run once at the start
-echo "Running filebot on startup..."
+echo "$(ts) Running filebot on startup..."
 run_filebot_sh
 
 # Wait forever for changes
 inotifywait -m -q -e close_write --format '%f' /input | while read FILE
 do
-  echo "Detected new file $FILE. Running FileBot."
+  echo "$(ts) Detected new file $FILE. Running FileBot."
 
   run_filebot_sh
 done
