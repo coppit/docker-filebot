6b651af0052d28f306290a6a993cbeb36d40b162
David Coppit <david@coppit.org>
2015-06-27 16:05:02 -0400

    Create config file and re-use monitor.sh

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,7 @@ RUN add-apt-repository ppa:webupd8team/java \
   && apt-get update \
   && apt-get install -y oracle-java8-installer
 
-# Create dir to keep things tidy. Make sure it's readable by $UID
+# Create dir to keep things tidy. Make sure it's readable by $USER_ID
 RUN mkdir /files
 RUN chmod a+rwX /files
 
@@ -32,16 +32,21 @@ VOLUME ["/input", "/output", "/config"]
 
 # Rev-locking this to ensure reproducible builds
 RUN wget -O /files/runas.sh \
-  'https://raw.githubusercontent.com/coppit/docker-inotify-command/dd981dc799362d47387da584e1a276bbd1f1bd1b/runas.sh'
+  'https://raw.githubusercontent.com/coppit/docker-inotify-command/7be05137c367a7bbff6b7980aa14e8af0c24eca6/runas.sh'
 RUN chmod +x /files/runas.sh
+RUN wget -O /files/monitor.sh \
+  'https://raw.githubusercontent.com/coppit/docker-inotify-command/7be05137c367a7bbff6b7980aa14e8af0c24eca6/monitor.sh'
+RUN chmod +x /files/monitor.sh
 
-# Add scripts. Make sure start.sh, pre-run.sh, and filebot.sh are executable by $UID
+# Add scripts. Make sure start.sh, pre-run.sh, and filebot.sh are executable by $USER_ID
 ADD pre-run.sh /files/pre-run.sh
 RUN chmod a+x /files/pre-run.sh
 ADD start.sh /files/start.sh
 RUN chmod a+x /files/start.sh
 ADD filebot.sh /files/filebot.sh
 RUN chmod a+wx /files/filebot.sh
+ADD filebot.conf /files/filebot.conf
+RUN chmod a+w /files/filebot.conf
 
 ENV USER_ID 0
 ENV GROUP_ID 0
--- start.sh
+++ start.sh
@@ -1,106 +1,83 @@
 #!/bin/bash
 
-# After seeing the first event, don't run FileBot until we've processed any other events that might be happening.
-# Otherwise we'll end up calling FileBot 100 times for the case where someone moves 100 files into the input directory.
-
-# If we don't see any events for $SETTLE_DURATION time, assume that it's safe to run FileBot
-SETTLE_DURATION=5
-
-# However, if we see continuous events for longer than $MAX_WAIT_TIME with no break of $SETTLE_DURATION or more, then
-# go ahead and run FileBot. Otherwise we might be waiting forever for the directory to stop getting events.
-MAX_WAIT_TIME=60
-
 function ts {
   echo [`date '+%b %d %X'`]
 }
 
-echo "$(ts) Starting FileBot container"
+#-----------------------------------------------------------------------------------------------------------------------
 
-# Create filebot.sh unless there is already one
-if [ ! -f /config/filebot.sh ]; then
-  echo "$(ts) Creating /config/filebot.sh and exiting"
-  cp /files/filebot.sh /config/filebot.sh
-  exit 1
-fi
+function initialize_configuration {
+  if [ ! -f /config/filebot.conf ]
+  then
+    echo "$(ts) Creating /config/filebot.conf"
+    cp /files/filebot.conf /config/filebot.conf
+    chmod a+w /config/filebot.conf
+  fi
 
-USER_VERSION=$(grep '^VERSION=' /config/filebot.sh 2>/dev/null | sed 's/VERSION=//')
-CURRENT_VERSION=$(grep '^VERSION=' /files/filebot.sh | sed 's/VERSION=//')
-
-echo "$(ts) Comparing user's filebot.sh at version $USER_VERSION versus current version $CURRENT_VERSION"
-
-if [ -z "$USER_VERSION" ] || [ "$USER_VERSION" -lt "$CURRENT_VERSION" ]
-then
-  echo "$(ts) ERROR: The container's filebot.sh is newer than the one in /config."
-  echo "$(ts)   Copying the new script to /config/filebot.sh.new."
-  echo "$(ts)   Compare your filebot.sh and filebot.sh.new. Save filebot.sh to reset its timestamp,"
-  echo "$(ts)   then restart the container."
-  cp /files/filebot.sh /config/filebot.sh.new
-  exit 1
-fi
-
-function run_filebot_sh {
-  # Do this every time in case the user edits their filebot.sh while the container is running
-  tr -d '\r' < /config/filebot.sh > /files/filebot.sh
-
-  # filebot amc's --log-file will implement locking, which will automatically slow this loop down if there are lots of
-  # events. https://www.filebot.net/forums/viewtopic.php?f=4&t=638
-  bash /files/filebot.sh
+  # Create filebot.sh unless there is already one
+  if [ ! -f /config/filebot.sh ]
+  then
+    echo "$(ts) Creating /config/filebot.sh and exiting"
+    cp /files/filebot.sh /config/filebot.sh
+    exit 1
+  fi
 }
 
-# Run once at the start
-echo "$(ts) Running filebot on startup"
-run_filebot_sh
+#-----------------------------------------------------------------------------------------------------------------------
 
-pipe=$(mktemp -u)
-mkfifo $pipe
+function check_filebot_sh_version {
+  USER_VERSION=$(grep '^VERSION=' /config/filebot.sh 2>/dev/null | sed 's/VERSION=//')
+  CURRENT_VERSION=$(grep '^VERSION=' /files/filebot.sh | sed 's/VERSION=//')
 
-echo "$(ts) Waiting for changes..."
-inotifywait -r -m -q --format '%e %f' /input >$pipe &
+  echo "$(ts) Comparing user's filebot.sh at version $USER_VERSION versus current version $CURRENT_VERSION"
 
-while true
-do
-  if read RECORD
+  if [ -z "$USER_VERSION" ] || [ "$USER_VERSION" -lt "$CURRENT_VERSION" ]
   then
-    EVENT=$(echo "$RECORD" | cut -d' ' -f 1)
-    FILE=$(echo "$RECORD" | cut -d' ' -f 2-)
-
-#    echo "$RECORD"
-#    echo "  EVENT=$EVENT"
-#    echo "  FILE=$FILE"
-
-    if [ "$EVENT" == "CREATE,ISDIR" ]
-    then
-      echo "$(ts) Detected new directory: $FILE"
-    elif [ "$EVENT" == "CLOSE_WRITE,CLOSE" ]
-    then
-      echo "$(ts) Detected new file: $FILE"
-    elif [ "$EVENT" == "MOVED_TO" ]
-    then
-      echo "$(ts) Detected moved file: $FILE"
-    else
-      continue
-    fi
-
-    # Monster up as many events as possible, until we hit the either the settle duration, or the max wait threshold.
-    start_time=$(date +"%s")
-
-    while true
-    do
-      if read -t $SETTLE_DURATION RECORD
-      then
-        end_time=$(date +"%s")
-
-        if [ $(($end_time-$start_time)) -gt $MAX_WAIT_TIME ]
-        then
-          echo "$(ts) Input directory didn't stabilize after $MAX_WAIT_TIME seconds. Running FileBot anyway."
-          break
-        fi
-      else
-        echo "$(ts) Input directory stabilized for $SETTLE_DURATION seconds. Running FileBot."
-        break
-      fi
-    done
-
-    run_filebot_sh
+    echo "$(ts) ERROR: The container's filebot.sh is newer than the one in /config."
+    echo "$(ts)   Copying the new script to /config/filebot.sh.new."
+    echo "$(ts)   Compare your filebot.sh and filebot.sh.new. Save filebot.sh to reset its timestamp,"
+    echo "$(ts)   then restart the container."
+    cp /files/filebot.sh /config/filebot.sh.new
+    exit 1
   fi
-done <$pipe
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+function create_conf_and_sh_files {
+  # Create the config file for monitor.sh
+  tr -d '\r' < /config/filebot.conf > /files/FileBot.conf
+
+  cat <<"EOF" >> /files/FileBot.conf
+
+WATCH_DIR=/input
+
+COMMAND="bash /files/filebot.sh"
+
+IGNORE_EVENTS_WHILE_COMMAND_IS_RUNNING=0
+
+USER_ID=$USER_ID
+GROUP_ID=$GROUP_ID
+UMASK=$UMASK
+EOF
+
+  # Strip \r from the user-provided filebot.sh
+  tr -d '\r' < /config/filebot.sh > /files/filebot.sh
+}
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+echo "$(ts) Starting FileBot container"
+
+initialize_configuration
+
+check_filebot_sh_version
+
+create_conf_and_sh_files
+
+# Run once at the start
+echo "$(ts) Running FileBot on startup"
+bash /files/filebot.sh
+
+# Start monitoring
+/files/monitor.sh /files/FileBot.conf
