8cb8588b32898974e1e5e783e71e0734c5a18a71
David Coppit <david@coppit.org>
2018-01-21 10:18:00 -0500

    Add USE_UI configuration option
    Thanks to Wob76 on the unraid forums for the suggestion.

--- startapp.sh
+++ startapp.sh
@@ -10,5 +10,11 @@ function ts {
 
 export DISPLAY=:1
 
-echo "$(ts) Running FileBot GUI"
-/files/runas.sh $USER_ID $GROUP_ID $UMASK filebot
+if [ "$RUN_UI" == "yes" ]
+then
+  echo "$(ts) Running FileBot GUI"
+  /files/runas.sh $USER_ID $GROUP_ID $UMASK filebot
+else
+  echo "$(ts) Not running FileBot GUI"
+  sv stop guacd tomcat7 filebot-ui X11rdp xrdp xrdp-sesman openbox
+fi
